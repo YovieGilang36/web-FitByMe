e2e57eab9c3717ff168a3dbebd09d3a950bac2f1
sitirohilah <99700683+sitirohilah@users.noreply.github.com>
2022-12-31 21:06:37 +0700

    Update nutrisi.php

--- nutrisi.php
+++ nutrisi.php
@@ -163,7 +163,7 @@ if ($gambar_produk != "") {
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Siti Rohilah</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
                                 <img class="img-profile rounded-circle"
                                     src="img/face1.jpg">
                             </a>
